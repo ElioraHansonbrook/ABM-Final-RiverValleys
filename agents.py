3a6171dd3e061bbf001eb376ed54652e5a9acc3e
ElioraHansonbrook <112485247+ElioraHansonbrook@users.noreply.github.com>
2026-05-16 10:14:02 -0400

    Expanded draft

--- agents.py
+++ agents.py
@@ -2,5 +2,22 @@ from mesa import Agent
 from model import RiverValley
 
 class Person(Agent):
-    def __init__(self, model: RiverValley, *args, **kwargs) -> None:
+    def __init__(self,
+                model: RiverValley,
+                preference = (0.999, 0.001), # Food acquisition preference
+                age = 0,
+                lifeExpectancy = 80,
+                *args,
+                **kwargs) -> None:
         super().__init__(model, *args, **kwargs)
+        self.preference = preference
+        self.age = age
+        self.lifeExpectancy = lifeExpectancy
+        self.food = (0,0)
+    
+    # Update the preference of the model as the first step in a round.
+    def updatePreference(self):
+        pass
+
+    def collectFood(self):
+        pass
--- model.py
+++ model.py
@@ -1,7 +1,39 @@
 from typing import Any
 
 from mesa import Model
+from mesa.space import HexMultiGrid
+#IMPORT RANDOM GENERATOR
 
+# A model of a river valley
 class RiverValley(Model):
-    def __init__(self, *args: Any, seed: float | None = None, rng: Generator | BitGenerator | int | integer[Any] | Sequence[int] | SeedSequence | None = None, scenario: Any | None = None, **kwargs: Any) -> None:
-        super().__init__(*args, seed=seed, rng=rng, scenario=scenario, **kwargs)
+    def __init__(self,
+                *args: Any,
+                seed: float | None = None,
+                rng: int | None = None,
+                scenario: Any | None = None,
+                height: int = 5,
+                width: int = 5,
+                agentCount: int = 25,
+                ) -> None:
+        super().__init__(*args, seed=seed, rng=rng, scenario=scenario,)
+        self.height = height
+        self.width = width
+        self.grid = HexMultiGrid(width = width, height = height, torus = False)
+        self.generateTileFertility(0) #FIX AND REPLACE WITH GENERATOR
+        self.assignAgents(agentCount)
+
+    # Set the fertility of each individual tile, surrounding the river.
+    def generateTileFertility(self, generator):
+        pass
+
+    # Provide the initial assignments of agents to tiles
+    def assignAgents(self, count):
+        pass
+
+    # A single step of the model
+    def step(self):
+        pass
+
+#Test sequence
+if __name__ == "__main__":
+    model = RiverValley(height=5, width=5)
